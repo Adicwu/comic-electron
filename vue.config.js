b2f5178d509e3cea0699a0f893c02d9aeb5e825c
adicwu <youzaiwu@qq.com>
2022-06-10 17:51:45 +0800

    .

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 const path = require('path')
 const resolve = (dir) => path.join(__dirname, dir)
 module.exports = defineConfig({
-  transpileDependencies: true,
+  publicPath: './',
   chainWebpack: (config) => {
     config.resolve.alias
       .set('~styles', resolve('./src/assets/css'))
